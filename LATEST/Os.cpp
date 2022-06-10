fcd1778c6dbaf29358edecd5706aa80920f24040
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-10 13:34:34 +0000

    Update: AUTOSAR basic software service module OS

--- LATEST/Os.cpp
+++ LATEST/Os.cpp
@@ -76,10 +76,6 @@ CONSTP2VAR(infOs_EcuM,    OS_VAR, OS_CONST) gptrinfOs_EcuM       = &Os;
 /******************************************************************************/
 VAR(module_Os, OS_VAR) Os(
    {
-#if(STD_ON == _ReSIM)
-         "Os",
-#else
-#endif
          OS_AR_RELEASE_VERSION_MAJOR
       ,  OS_AR_RELEASE_VERSION_MINOR
       ,  0x00
